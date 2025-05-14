ce4719c553b4189e99dfcd048f678751c48b597c
Omimin033 <105780711+Omimin033@users.noreply.github.com>
2025-05-14 12:09:08 +0900

    6章：Moneyクラスを作成、Dollarに継承

--- money/Dollar.java
+++ money/Dollar.java
@@ -1,6 +1,6 @@
 package money;  //moneyパッケージを作成
 
-class Dollar {  //Dollarクラスを作成
+class Dollar extends Money {  //Dollarクラスを作成
     private int amount;         //amountフィールドを作成
     Dollar(int amount) {        //コンストラクタを作成
         this.amount = amount;   //引数のamountをフィールドに代入
