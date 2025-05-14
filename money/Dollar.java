ad7e6f6124b193ca315ffd8f61cd1d08af8743d1
Omimin033 <105780711+Omimin033@users.noreply.github.com>
2025-05-14 12:11:15 +0900

    ６章：amountをMoneyクラスの変数に

--- money/Dollar.java
+++ money/Dollar.java
@@ -1,7 +1,6 @@
 package money;  //moneyパッケージを作成
 
 class Dollar extends Money {  //Dollarクラスを作成
-    private int amount;         //amountフィールドを作成
     Dollar(int amount) {        //コンストラクタを作成
         this.amount = amount;   //引数のamountをフィールドに代入
     }
@@ -13,3 +12,21 @@ class Dollar extends Money {  //Dollarクラスを作成
         return amount == dollar.amount;         //amountフィールドを比較
     }
 }
+
+
+
+
+// ５章までのコード
+// class Dollar {  //Dollarクラスを作成
+//     private int amount;         //amountフィールドを作成
+//     Dollar(int amount) {        //コンストラクタを作成
+//         this.amount = amount;   //引数のamountをフィールドに代入
+//     }
+//     Dollar times(int multiplier) {  //timesメソッドを作成
+//         return new Dollar(amount * multiplier);  //新しいDollarオブジェクトを返す
+//     }
+//     public boolean equals(Object object) {      //equalsメソッドをオーバーライド
+//         Dollar dollar = (Dollar) object;        //Object型をDollar型にキャスト
+//         return amount == dollar.amount;         //amountフィールドを比較
+//     }
+// }
--- money/Money.java
+++ money/Money.java
@@ -1,5 +1,5 @@
 package money;
 
 class Money {
-    
+    protected int amount;   //サブクラスでアクセスできるようにprotected修飾子を付ける
 }
