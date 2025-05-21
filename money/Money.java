f209959ddbbe9116a22b4f36ce9bfe4ecb7fadbb
Omimin033 <105780711+Omimin033@users.noreply.github.com>
2025-05-21 11:52:58 +0900

    ９章：Moneyクラスに抽象メソッド「通貨」を実装

--- money/Money.java
+++ money/Money.java
@@ -3,6 +3,7 @@ package money;
 abstract class Money {
     protected int amount;   //サブクラスでアクセスできるようにprotected修飾子を付ける
     abstract Money times(int multiplier);    //抽象メソッドを作成
+    abstract String currency();    //抽象メソッド「通貨」を作成
     public boolean equals(Object object) {   //equalsメソッドをオーバーライド
         Money money = (Money) object;        //Object型をDollar型にキャスト
         return amount == money.amount        //amountフィールドを比較
