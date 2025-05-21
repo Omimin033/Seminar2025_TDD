a1e5fce84d91e41b93afc68cdb8e24610c30a82e
Omimin033 <105780711+Omimin033@users.noreply.github.com>
2025-05-21 10:57:37 +0900

    ７章：2つのMoneyオブジェクトのクラスが等しい場合に値が等しくなるように

--- money/Money.java
+++ money/Money.java
@@ -4,6 +4,7 @@ class Money {
     protected int amount;   //サブクラスでアクセスできるようにprotected修飾子を付ける
     public boolean equals(Object object) {   //equalsメソッドをオーバーライド
         Money money = (Money) object;        //Object型をDollar型にキャスト
-        return amount == money.amount;       //amountフィールドを比較
+        return amount == money.amount        //amountフィールドを比較
+            && getClass().equals(money.getClass()); //クラスを比較
     }
 }
