e2b9c8e81c9480bdafd83b556e9d0777100d1f5b
Omimin033 <105780711+Omimin033@users.noreply.github.com>
2025-05-21 11:32:05 +0900

    ８章：残りの箇所もFactory Methodを使うようにテストを変更

--- money/MoneyTest.java
+++ money/MoneyTest.java
@@ -8,16 +8,16 @@ public class MoneyTest {    //MoneyTestクラスを作成
     public void testMultiplication() {              //testMultiplicationメソッドを作成
         Money five = Money.dollar(5);         //Dollarクラスのインスタンスを作成
         // Dollar同士を比較していることをわかりやすくするために、assertEqualsの引数を変更
-        assertEquals(new Dollar(10), five.times(2));  //timesメソッドを呼び出す
-        assertEquals(new Dollar(15), five.times(3));  //timesメソッドを呼び出す
+        assertEquals(Money.dollar(10), five.times(2));  //timesメソッドを呼び出す
+        assertEquals(Money.dollar(15), five.times(3));  //timesメソッドを呼び出す
         }
     @Test
     public void testEquality() {    //testEqualityメソッドを作成
-        assertTrue(new Dollar(5).equals(new Dollar(5)));  //assertTrueメソッドで期待値と実際の値を比較
-        assertFalse(new Dollar(5).equals(new Dollar(6))); //assertFalseメソッドで期待値と実際の値を比較
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));  //assertTrueメソッドで期待値と実際の値を比較
+        assertFalse(Money.dollar(5).equals(Money.dollar(6))); //assertFalseメソッドで期待値と実際の値を比較
         assertTrue(new Franc(5).equals(new Franc(5)));    //assertTrueメソッドで期待値と実際の値を比較
         assertFalse(new Franc(5).equals(new Franc(6)));   //assertFalseメソッドで期待値と実際の値を比較
-        assertFalse(new Franc(5).equals(new Dollar(5)));   //assertFalseメソッドで期待値と実際の値を比較
+        assertFalse(new Franc(5).equals(Money.dollar(5)));   //assertFalseメソッドで期待値と実際の値を比較
     }
     @Test
     public void testFrancMultiplication() {              //testFrancMultiplicationメソッドを作成
