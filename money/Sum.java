9abdecad3a459f1972d9fdac09c362ea79262f25
Omimin033 <105780711+Omimin033@users.noreply.github.com>
2025-06-04 11:49:35 +0900

    Sumのコンストラクタで引数をフィールドにいれる

--- money/Sum.java
+++ money/Sum.java
@@ -4,5 +4,7 @@ class Sum implements Expression {  //Sumクラスを作成し、Expressionイン
     Money augend;  //加算される金額
     Money addend;  //加算する金額
     Sum(Money augend, Money addend) {  //コンストラクタを作成
+        this.augend = augend;  //引数のaugendをフィールドに代入
+        this.addend = addend;  //引数のaddendをフィールドに代入
     }
 }
