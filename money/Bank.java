e41f040312fea0719950ed72338dec9d8f16355d
Omimin033 <105780711+Omimin033@users.noreply.github.com>
2025-06-04 12:16:03 +0900

    キャストやクラスのチェックをすべて排除

--- money/Bank.java
+++ money/Bank.java
@@ -2,9 +2,6 @@ package money;
 
 class Bank {
     Money reduce(Expression source, String to) {
-        if (source instanceof Money)  //sourceがMoney型かどうかをチェック
-            return ((Money) source).reduce(to);  //Money型ならreduceメソッドを呼び出す
-        Sum sum = (Sum) source;
-        return sum.reduce(to);  //Sumオブジェクトをto通貨に変換して返す
+        return source.reduce(to);  //Sumオブジェクトをto通貨に変換して返す
     }
 }
