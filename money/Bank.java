9d9cfbb9256d6c1772795951da7d9cc070a32e4d
Omimin033 <105780711+Omimin033@users.noreply.github.com>
2025-06-04 12:09:08 +0900

    Bank側でインスタンスがMoneyだったらMoneyを返すように修正

--- money/Bank.java
+++ money/Bank.java
@@ -2,6 +2,8 @@ package money;
 
 class Bank {
     Money reduce(Expression source, String to) {
+        if (source instanceof Money)  //sourceがMoney型かどうかをチェック
+            return (Money) source;  //Money型の場合はそのまま返す
         Sum sum = (Sum) source;
         return sum.reduce(to);  //Sumオブジェクトをto通貨に変換して返す
     }
