6947d6d0862aba29b41c09abc1fbbfcb008f7507
Omimin033 <105780711+Omimin033@users.noreply.github.com>
2025-06-18 11:56:03 +0900

    fiveBucks変数の型をExpressionに変更（テスト×）

--- money/MoneyTest.java
+++ money/MoneyTest.java
@@ -64,7 +64,7 @@ public class MoneyTest {    //MoneyTestクラスを作成
     }
     @Test
     public void testMixedAddition() {
-        Money fiveBucks = Money.dollar(5);
+        Expression fiveBucks = Money.dollar(5);
         Expression tenFrancs = Money.franc(10);
         Bank bank = new Bank();
         bank.addRate("CHF", "USD", 2);
