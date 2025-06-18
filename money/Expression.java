c0ce8be9b281b4f203ccc79804a31db52d9d295f
Omimin033 <105780711+Omimin033@users.noreply.github.com>
2025-06-18 12:17:34 +0900

    Expressionインターフェースにtimesメソッドを宣言（前の章でaugendとaddendフィールドの型をExpressionまで抽象化したため）（テスト×）

--- money/Expression.java
+++ money/Expression.java
@@ -1,6 +1,7 @@
 package money;
 
 interface Expression {
+    Expression times(int multiplier);  // Expressionインターフェースにtimesメソッドを追加
     Expression plus(Expression addend);  // Expressionインターフェースにplusメソッドを追加
     Money reduce(Bank bank,  String to);  // Expressionインターフェースにreduceメソッドを追加
 }
