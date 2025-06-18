9c054bf83e8d515e1c87797f3c4af26c8b253538
Omimin033 <105780711+Omimin033@users.noreply.github.com>
2025-06-18 10:51:38 +0900

    コンパイラを通すために、BankにaddRateメソッドの空実装を定義

--- money/Bank.java
+++ money/Bank.java
@@ -4,4 +4,7 @@ class Bank {
     Money reduce(Expression source, String to) {
         return source.reduce(to);  //Sumオブジェクトをto通貨に変換して返す
     }
+    void addRate(String from, String to, int rate) {
+        // 為替レートを追加するメソッド（実装は省略）
+    }
 }
