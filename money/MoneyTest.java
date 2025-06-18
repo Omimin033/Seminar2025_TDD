00017c59810259e8bff37e7006a3449e847d52e5
Omimin033 <105780711+Omimin033@users.noreply.github.com>
2025-06-18 11:54:15 +0900

    テストケースでlusに渡している引数の型をExpressionに変更（テストOK）

--- money/MoneyTest.java
+++ money/MoneyTest.java
@@ -65,7 +65,7 @@ public class MoneyTest {    //MoneyTestクラスを作成
     @Test
     public void testMixedAddition() {
         Money fiveBucks = Money.dollar(5);
-        Money tenFrancs = Money.franc(10);
+        Expression tenFrancs = Money.franc(10);
         Bank bank = new Bank();
         bank.addRate("CHF", "USD", 2);
         Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
