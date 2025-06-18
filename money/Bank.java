51494bbab1c0992177e8367cb57a2428a77c407a
Omimin033 <105780711+Omimin033@users.noreply.github.com>
2025-06-18 10:54:51 +0900

    呼び出し側でBankを渡すように変更

--- money/Bank.java
+++ money/Bank.java
@@ -2,7 +2,7 @@ package money;
 
 class Bank {
     Money reduce(Expression source, String to) {
-        return source.reduce(to);  //Sumオブジェクトをto通貨に変換して返す
+        return source.reduce(this, to);  //Sumオブジェクトをto通貨に変換して返す
     }
     void addRate(String from, String to, int rate) {
         // 為替レートを追加するメソッド（実装は省略）
