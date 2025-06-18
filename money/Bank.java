23ca7b9ee54c75092d7beb27727ff9f47e1a0081
Omimin033 <105780711+Omimin033@users.noreply.github.com>
2025-06-18 11:19:35 +0900

    為替レート設定時に格納を行う

--- money/Bank.java
+++ money/Bank.java
@@ -9,7 +9,7 @@ class Bank {
         return source.reduce(this, to);  //Sumオブジェクトをto通貨に変換して返す
     }
     void addRate(String from, String to, int rate) {
-        
+        rates.put(new Pair(from, to), rate);    //為替レートをMapに追加
     }
     int rate(String from, String to) {
         return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;  //為替レートを設定
