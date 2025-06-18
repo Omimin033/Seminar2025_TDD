801b415d8de7a01f098336ef63cd55a633674e1d
Omimin033 <105780711+Omimin033@users.noreply.github.com>
2025-06-18 11:40:16 +0900

    Sumのコントラクタ引数もExpressionに変更（テストOK）

--- money/Sum.java
+++ money/Sum.java
@@ -3,7 +3,7 @@ package money;
 class Sum implements Expression {  //Sumクラスを作成し、Expressionインターフェースを実装
     Expression augend;  //加算される金額
     Expression addend;  //加算する金額
-    Sum(Money augend, Money addend) {  //コンストラクタを作成
+    Sum(Expression augend, Expression addend) {  //コンストラクタを作成
         this.augend = augend;  //引数のaugendをフィールドに代入
         this.addend = addend;  //引数のaddendをフィールドに代入
     }
