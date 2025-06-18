229967720381fd880569527e078431dccbef5a28
Omimin033 <105780711+Omimin033@users.noreply.github.com>
2025-06-18 11:39:31 +0900

    テストが通るようになったので、依存度の低い場所からMoneyをExpressionに置き換え（まずはSumのaugendとaddend）

--- money/Sum.java
+++ money/Sum.java
@@ -1,8 +1,8 @@
 package money;
 
 class Sum implements Expression {  //Sumクラスを作成し、Expressionインターフェースを実装
-    Money augend;  //加算される金額
-    Money addend;  //加算する金額
+    Expression augend;  //加算される金額
+    Expression addend;  //加算する金額
     Sum(Money augend, Money addend) {  //コンストラクタを作成
         this.augend = augend;  //引数のaugendをフィールドに代入
         this.addend = addend;  //引数のaddendをフィールドに代入
